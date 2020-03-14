4b94f335c58e1de786296f39d8e8bad290a42064
sandeepvalluri14 <54743374+sandeepvalluri14@users.noreply.github.com>
2020-03-14 22:14:51 +0530

    Update maths.sh

--- maths.sh
+++ maths.sh
@@ -5,9 +5,10 @@ c=$((a * a + b * b + 2 * a * b))
 echo "(a + b)2=" $c
 d=$((a * a +b *b - 2 * a * b))
 echo "(a-b)2=" $d
-if (c > 20)
-{   
-echo "value is greater than 20"
+
+if [ c > 20 ]
+then 
+   echo "value is greater than 20"
 else
-echo "value is less than 20"
-}
+   echo "value is less than 20"
+fi
